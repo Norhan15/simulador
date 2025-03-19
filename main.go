c77a4a7e873778a27466e6a96b28ed042c5f29f5
Norma Serrano <normamarlithsm15@gmail.com>
2025-03-18 19:18:11 -0600

    cambios

--- main.go
+++ main.go
@@ -1,26 +1,54 @@
 package main
 
 import (
-	"simulador/src/scenes"
-	"simulador/src/views"
+	"fmt"
 
-
-	"fyne.io/fyne/v2"
+    "fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Parking Scene")
+    myApp := app.New()
+    myWindow := myApp.NewWindow("Ejemplo Completo")
+    myWindow.Resize(fyne.NewSize(400, 300))
 
-	parkingScene := scenes.NewParkingScene(myWindow)
-	parkingScene.Init()
+    // Etiqueta para mensajes
+    messageLabel := widget.NewLabel("Seleccione una opción:")
 
-	carObserver := views.NewCarObserver(parkingScene.Car, parkingScene)
+    // Radio Group
+    radio := widget.NewRadioGroup([]string{"Opción A", "Opción B"}, func(selected string) {
+        messageLabel.SetText(fmt.Sprintf("Seleccionaste: %s", selected))
+    })
 
-    // Registrar el observador
-    parkingScene.Register(carObserver)
+    // Slider
+    sliderValue := widget.NewLabel("Valor del slider: 0.0")
+    slider := widget.NewSlider(0, 10)
+    slider.OnChanged = func(value float64) {
+        sliderValue.SetText(fmt.Sprintf("Valor del slider: %.1f", value))
+    }
 
-	myWindow.Resize(fyne.NewSize(900, 600))
-	myWindow.ShowAndRun()
+    // Botón con mensaje
+    resultLabel := widget.NewLabel("")
+    button := widget.NewButton("Enviar", func() {
+        if radio.Selected == "" {
+            resultLabel.SetText("Por favor, selecciona una opción.")
+        } else {
+            resultLabel.SetText(fmt.Sprintf("Enviado: %s con valor %.1f", radio.Selected, slider.Value))
+        }
+    })
+
+    // Contenedor
+    content := container.NewVBox(
+        messageLabel,
+        radio,
+        slider,
+        sliderValue,
+        button,
+        resultLabel,
+    )
+
+    myWindow.SetContent(content)
+    myWindow.ShowAndRun()
 }
